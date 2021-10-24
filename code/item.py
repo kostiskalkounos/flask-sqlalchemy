32ecd79617aa9387fe406d5f8d3c85f9346972c0
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-24 14:58:34 +0200

    write items to the db

--- code/item.py
+++ code/item.py
@@ -10,8 +10,8 @@ class Item(Resource):
                          help='This field cannot be left blank.'
     )
 
-    @jwt_required()
-    def get(self, name):
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -22,16 +22,32 @@ class Item(Resource):
 
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
+
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
         return {'message': 'Item not found'}, 404
 
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
             return {'message': "An Item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
 
     @jwt_required()
