1bb154ba843c7ad2af9ea1347b03adb6794a5d71
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-24 13:56:12 +0200

    log and retrieve users from sqlite

--- code/security.py
+++ code/security.py
@@ -1,12 +1,5 @@
 from user import User
 
-users = [
-    User(1, 'Bob', 'asdf')
-]
-
-username_mapping = { u.username: u for u in users }
-userid_mapping = { u.id: u for u in users }
-
 userid_mapping = { 1: {
         'id': 1,
         'username': 'bob',
@@ -15,10 +8,10 @@ userid_mapping = { 1: {
 }
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and user.password == password:
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
