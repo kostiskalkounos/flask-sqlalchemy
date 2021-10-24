f3f43b706b5e4a1410a89bbf610532a846d4bc03
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-24 15:05:39 +0200

    delete item from the db

--- code/item.py
+++ code/item.py
@@ -52,8 +52,15 @@ class Item(Resource):
 
     @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
         return {'message': 'Item deleted'}
 
     @jwt_required()
