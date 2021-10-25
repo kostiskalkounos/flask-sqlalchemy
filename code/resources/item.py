c43ff34765efea41556b97dca79338f80882409a
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-25 11:58:56 +0200

    create StoreModel

--- code/resources/item.py
+++ code/resources/item.py
@@ -2,12 +2,19 @@ from models.item import ItemModel
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 
+
 class Item(Resource):
     parser = reqparse.RequestParser()
-    parser.add_arguement('price',
+    parser.add_argument('price',
                          type=float,
                          required=True,
                          help='This field cannot be left blank.'
+    )
+
+    parser.add_argument('store_id',
+                         type=int,
+                         required=True,
+                         help='Every item needs a store id.'
     )
 
     @jwt_required()
@@ -23,7 +30,7 @@ class Item(Resource):
             return {'message': "An Item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -46,7 +53,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
